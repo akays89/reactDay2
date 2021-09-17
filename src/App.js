18cddf3115a091c9eb024e74693adc2fb0c46a09
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-09-16 19:41:35 -0500

    timer running

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React, { useEffect, useState } from "react";
+
 function App() {
-  return <main />;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCount(count + 1);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
+  return <main>{count}</main>;
 }
 
 export default App;
